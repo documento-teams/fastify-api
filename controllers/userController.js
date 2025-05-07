18531079ff52b53e14b0598cc2685cf102c4ab82
Rémy <remy.berjonneau@gmail.com>
2025-05-07 10:46:46 +0200

    refactor: update user authentication logic delete user and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
@@ -16,12 +16,17 @@ const userController = {
         return reply.status(400).send({ message: "All fields are required" });
       }
 
-      const user = await prisma.user.findUnique({
-        where: { email },
+      const user = await prisma.user.findFirst({
+        where: {
+          email: email,
+        },
+        orderBy: {
+          id: "asc",
+        },
       });
 
       if (!user) {
-        return reply.status(401).send({ message: "Invalid credentials" });
+        return reply.status(401).send({ message: "Email is not registered" });
       }
 
       const isPasswordValid = await bcrypt.compare(password, user.password);
@@ -42,17 +47,20 @@ const userController = {
   },
   register: async (request, reply) => {
     try {
-      const { name, email, password } = request.body;
+      const { fullname, email, password } = request.body;
 
-      if (!name || !email || !password) {
+      if (!fullname || !email || !password) {
         return reply
           .status(400)
           .send({ message: "Tous les champs sont requis" });
       }
 
-      const userExisting = await prisma.user.findUnique({
+      const userExisting = await prisma.user.findFirst({
         where: {
           email: email,
+        },
+        orderBy: {
+          id: "asc",
         },
       });
 
@@ -66,7 +74,7 @@ const userController = {
 
       const user = await prisma.user.create({
         data: {
-          name,
+          fullname,
           email,
           password: hashedPassword,
         },
@@ -79,7 +87,7 @@ const userController = {
       return reply.status(201).send({
         user: {
           id: user.id,
-          name: user.name,
+          fullname: user.fullname,
           email: user.email,
         },
         token,
@@ -89,6 +97,69 @@ const userController = {
       return reply.status(500).send({ message: "Erreur interne du serveur" });
     }
   },
+  getme: async (request, reply) => {
+    try {
+      const userId = request.user.userId;
+
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        return reply.status(404).send({ message: "User not found" });
+      }
+
+      return reply.status(200).send({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      });
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      return reply.status(500).send({ message: "Internal server error" });
+    }
+  },
+  deleteUser: async (request, reply ) => {
+    try {
+      const userId = request.body.id;
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+      })
+      if (!user) {
+        return reply.status(404).send({ message: "User not found" });
+      }
+      await prisma.user.delete({
+        where: { id: userId },
+      });
+      return reply.status(200).send({ message: "User deleted successfully" });
+    }catch (error) {
+      console.error("Error deleting user:", error);
+      return reply.status(500).send({ message: "Internal server error" });
+    }
+  },
+  updateUser: async (request , reply) => {
+    try{
+    const userId = request.body.id
+    const { fullname, email } = request.body;
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      return reply.status(404).send({ message: "User not found" });
+    }
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        fullname: fullname || user.fullname,
+        email: email || user.email,
+      },
+    })
+    return reply.status(200).send({ message: "User updated successfully"});
+  }catch (error) {
+    console.error("Error updating user:", error);
+    return reply.status(500).send({ message: "Internal server error" });
+  }
+}
 };
 
 export default userController;
