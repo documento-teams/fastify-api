9d423729b0a774c9f7b2ee997c20f5a395d9540b
Rémy <remy.berjonneau@gmail.com>
2025-05-26 15:22:44 +0200

    fix route swagger

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -2,36 +2,112 @@ import documentController from "../controllers/documentController";
 import authenticate from "../middlewares/authMiddleware.js";
 
 const documentRoutes = (fastify) => {
-  fastify.post(
-    "/create",
-    { onRequest: authenticate },
-    documentController.createDocument
-  );
-  fastify.get(
-    "/all",
-    { onRequest: authenticate },
-    documentController.getAllDocuments
-  );
-  fastify.get(
-    "/author",
-    { onRequest: authenticate },
-    documentController.getDocumentByAuthorId
-  );
-  fastify.delete(
-    "/delete",
-    { onRequest: authenticate },
-    documentController.deleteDocument
-  );
-  fastify.put(
-    "/update",
-    { onRequest: authenticate },
-    documentController.updateDocument
-  );
-  fastify.get(
-    "/workspace/:id",
-    { onRequest: authenticate },
-    documentController.getDocumentsByWorkspaceId
-  );
+  fastify.post("/create", {
+    schema: {
+      tags: ["document"],
+      description: "Create a new document",
+      security: [{ cookieAuth: [] }],
+      body: {
+        type: "object",
+        required: ["name", "workspaceId"],
+        properties: {
+          name: { type: "string" },
+          content: { type: "string" },
+          workspaceId: { type: "integer" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: documentController.createDocument,
+  });
+
+  fastify.get("/all", {
+    schema: {
+      tags: ["document"],
+      description: "Get all documents",
+      security: [{ cookieAuth: [] }],
+    },
+    onRequest: authenticate,
+    handler: documentController.getAllDocuments,
+  });
+
+  fastify.get("/author", {
+    schema: {
+      tags: ["document"],
+      description: "Get documents for the current user",
+      security: [{ cookieAuth: [] }],
+    },
+    onRequest: authenticate,
+    handler: documentController.getDocumentByAuthorId,
+  });
+
+  fastify.delete("/delete", {
+    schema: {
+      tags: ["document"],
+      description: "Delete a document",
+      security: [{ cookieAuth: [] }],
+      body: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: { type: "integer" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: documentController.deleteDocument,
+  });
+
+  fastify.put("/update", {
+    schema: {
+      tags: ["document"],
+      description: "Update a document",
+      security: [{ cookieAuth: [] }],
+      body: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: { type: "integer" },
+          name: { type: "string" },
+          content: { type: "string" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: documentController.updateDocument,
+  });
+
+  fastify.get("/workspace/:id", {
+    schema: {
+      tags: ["document"],
+      description: "Get documents for a workspace",
+      security: [{ cookieAuth: [] }],
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "string" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: documentController.getDocumentsByWorkspaceId,
+  });
+
+  fastify.get("/:id", {
+    schema: {
+      tags: ["document"],
+      description: "Get a document by ID",
+      security: [{ cookieAuth: [] }],
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "string" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: documentController.getDocumentById,
+  });
 };
 
 export default documentRoutes;
--- routes/workspaceRoutes.js
+++ routes/workspaceRoutes.js
@@ -2,36 +2,95 @@ import workspaceController from "../controllers/workspaceController.js";
 import authenticate from "../middlewares/authMiddleware.js";
 
 const workspaceRoutes = (fastify) => {
-  fastify.post(
-    "/create",
-    { onRequest: authenticate },
-    workspaceController.createWorkspace
-  );
-  fastify.get(
-    "/all",
-    { onRequest: authenticate },
-    workspaceController.getAllWorkspaces
-  );
-  fastify.get(
-    "/author",
-    { onRequest: authenticate },
-    workspaceController.getWorkspaceByAuthorId
-  );
-  fastify.delete(
-    "/delete",
-    { onRequest: authenticate },
-    workspaceController.deleteWorkspace
-  );
-  fastify.put(
-    "/update",
-    { onRequest: authenticate },
-    workspaceController.updateWorkspace
-  );
-  fastify.get(
-    "/:id",
-    { onRequest: authenticate },
-    workspaceController.getWorkspaceById
-  );
+  fastify.post("/create", {
+    schema: {
+      tags: ["workspace"],
+      description: "Create a new workspace",
+      security: [{ cookieAuth: [] }],
+      body: {
+        type: "object",
+        required: ["name"],
+        properties: {
+          name: { type: "string" },
+          description: { type: "string" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: workspaceController.createWorkspace,
+  });
+
+  fastify.get("/all", {
+    schema: {
+      tags: ["workspace"],
+      description: "Get all workspaces",
+      security: [{ cookieAuth: [] }],
+    },
+    onRequest: authenticate,
+    handler: workspaceController.getAllWorkspaces,
+  });
+
+  fastify.get("/author", {
+    schema: {
+      tags: ["workspace"],
+      description: "Get workspaces for the current user",
+      security: [{ cookieAuth: [] }],
+    },
+    onRequest: authenticate,
+    handler: workspaceController.getWorkspaceByAuthorId,
+  });
+
+  fastify.delete("/delete", {
+    schema: {
+      tags: ["workspace"],
+      description: "Delete a workspace",
+      security: [{ cookieAuth: [] }],
+      body: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: { type: "integer" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: workspaceController.deleteWorkspace,
+  });
+
+  fastify.put("/update", {
+    schema: {
+      tags: ["workspace"],
+      description: "Update a workspace",
+      security: [{ cookieAuth: [] }],
+      body: {
+        type: "object",
+        required: ["id"],
+        properties: {
+          id: { type: "integer" },
+          name: { type: "string" },
+          description: { type: "string" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: workspaceController.updateWorkspace,
+  });
+
+  fastify.get("/:id", {
+    schema: {
+      tags: ["workspace"],
+      description: "Get a workspace by ID",
+      security: [{ cookieAuth: [] }],
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "string" },
+        },
+      },
+    },
+    onRequest: authenticate,
+    handler: workspaceController.getWorkspaceById,
+  });
 };
 
 export default workspaceRoutes;
